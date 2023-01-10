9907cca6b2892a2e7b43edcc0f51705e51146fee
Barbaros Teoman Kosoglu <btkkteoman@icloud.com>
2023-01-11 02:38:00 +0300

    day1

--- deck.py
+++ deck.py
@@ -6,11 +6,24 @@ class Deck:
     def __init__(self):
         pass
 
-    def create_deck(self):
-        pass
+    @staticmethod
+    def create_deck():
+        global allCards
+        allCards = []
+        for value1 in Card.SUIT_SYMBOLS.values():
+            for value2 in Card.VALUE_NAMES.values():
+                allCards.append(f"{value2} {value1}")
+        return allCards
 
-    def shuffle(self):
-        pass
+    @staticmethod
+    def shuffle():
+        Deck.create_deck()
+        random.shuffle(allCards)
+        return allCards
 
     def deal(self, num_cards):
         pass
+
+
+deck = Deck()
+print(deck.shuffle())
